7579b3169b565b038ba9f113e86a82ef562b7496
Madelaine Avendaño <madelaineavendano@gmail.com>
2026-06-30 18:57:20 -0400

    sprint2: implementacion de pipeline ETL, data cleaning y generación automatica de reportes

---
+++ funciones.py
@@ -0,0 +1,6 @@
+#definimos la funcion (construimos la maquina)
+def saludar_empleado(nombre):
+    print(f"hola {nombre}, bienvenido al turno de hoy.")
+
+saludar_empleado("Madelaine")
+saludar_empleado ("Victor")
--- trabajo.py
+++ trabajo.py
@@ -1,38 +1,75 @@
 import math
-
+import csv
+# =======================================================
+# 1. NUESTRAS FUNCIONES (Las herramientas)
+# =======================================================
+def calcular_personal_necesario(cantidad_deptos, capacidad_maxima):
+    if cantidad_deptos==0:
+        return 0
+    elif cantidad_deptos==1:
+        return 1
+    else :
+        #math.ceil redondea hacia arriba, +1 persona base
+        return math.ceil(cantidad_deptos/capacidad_maxima)+1
+# =======================================================
+# 2.EXTRACCION DE DATOS (LEYENDO DESDE ARCHIVO EXTERNO) 
+# =======================================================  
 #estado:"o"= ocupado, "r"= reposicion, "l"= dejar limpio
-estado_deptos= {
-    "102": "r", "118":"o", "123":"l", "147":"o", "156":"r", "160":"r", "161":"r", "162":"r",  "163":"r", "164":"r" 
-
-}
+estado_deptos={}
+# abrimos la puerta al archivo externo
+with open ("datos1.csv", mode="r", encoding="utf-8") as archivo:
+    #csv.DictReader lee la primera fila como nombres de las comlumnas
+    lector=csv.DictReader(archivo)
+    #recorremos el archivo fila por fila
+    for fila in lector:
+        depto= fila["departamento"].strip()
+        #limpieza intensiva de la columna estado
+        estado= fila["estado"].upper().strip()
+        #listas de estados autorizados para el negocio
+        estados_validos= ["R", "L", "O"]
+        if estado in estados_validos:
+            estado_deptos[depto]= estado
+        else: 
+            print(f"advertincia: el departamento {depto} tiene un estado invalido ('{estado} ')ignorando fila")
+print (estado_deptos)        
+# =======================================================
+# 3. LÓGICA DE CLASIFICACIÓN (El motor)
+# =======================================================
 total_reposicion=0
 total_dejar_listo=0
 #el bucle revisa y clasifica cada depto
 for depto, estado in estado_deptos.items():
-    if estado== "r":
+    if estado== "R":
         total_reposicion=total_reposicion+1
-    elif estado== "l":
+    elif estado== "L":
         total_dejar_listo+=1
+# =======================================================
+# 4. CÁLCULO Y RESULTADOS (Usando nuestra función)
+# =======================================================
 print(f"Total deptos con reposicion: {total_reposicion} y Total deptos dejar listos: { total_dejar_listo}")
- #algoritmo del personal para dejar listo (capacidad max=8) 
-if total_dejar_listo==0:
-    personal_l=0
-elif total_dejar_listo==1:
-    personal_l=1 
-else:
-    personal_l= math.ceil (total_dejar_listo /8)+1
-
-#algoritmo del personal para reposicion (capacidad max=6)
-if total_reposicion==0:
-    personal_r=0
-elif total_reposicion==1:
-    personal_r=1 
-else:
-    # si hay entre 2 y 6 deptos, math.ceil da 1. mas 1 base = 2 personas
-    # si hay 7 deptos, math.ceil da 2. mas 1 de base = 3 personas
-    personal_r= math.ceil (total_reposicion /6)+1
-
+# Llamamos a nuestra función dos veces enviándole parámetros diferentes
+personal_r= calcular_personal_necesario(total_reposicion, 6)
+personal_l= calcular_personal_necesario(total_dejar_listo, 8)
+personal_total= personal_l+ personal_r
+#creamos una variable con el texto completo del reporte
+reporte_final= f"""
+=========================================
+      REPORTE DIARIO DE LOGÍSTICA
+=========================================
+Departamentos reposicion: {total_reposicion} (requiere {personal_r} personas)
+Departamentos dejar listos: {total_dejar_listo} (requiere {personal_l} personas)
+-----------------------------------------
+TOTAL DE TRABAJADORES PARA MAÑANA: {personal_total}
+=========================================
+"""
+print(reporte_final)
 print("\n--- REPORTE AUTOMÁTICO DE LOGÍSTICA ---")
 print(f"Departamentos Reposición: {total_reposicion} -> Personal: {personal_r} personas.")
 print(f"Departamentos Dejar Listo: {total_dejar_listo} -> Personal: {personal_l} personas.")
 #print(f"TOTAL DE TRABAJADORES PARA MAÑANA:")
+# =======================================================
+# 5. GENERACIÓN DEL ARCHIVO (CARGA DE DATOS)
+# =======================================================
+with open("reporte_para_maritza.txt", mode= "w", encoding= "utf-8") as archivo_salida:
+    archivo_salida.write(reporte_final)
+print("exito: el archivo 'reporte_para_maritza.txt' ha sido creado con exito" )
